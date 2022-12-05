c703025045686126c84247a0f8c8eabcf57b8f55
Mattbanana <matthiasbirchandersen@gmail.com>
2022-12-05 16:08:41 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Eksamensprojekt_2nd.Viewmodels;
+using Eksamensprojekt_2nd.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eksamensprojekt_2nd
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void project_view_button_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new Projects_viewmodel();
+        }
+
+        private void manager_view_button_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new Project_managers_viewmodel();
+        }
+    }
+}
---
+++ Viewmodels/Project_managers_viewmodel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eksamensprojekt_2nd.Viewmodels
+{
+    public class Project_managers_viewmodel
+    {
+    }
+}
