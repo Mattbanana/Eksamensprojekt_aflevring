dbea9f908bbe6877c29df71b913997aede07148b
Mattbanana <matthiasbirchandersen@gmail.com>
2022-12-05 20:11:29 +0100

    oprettet class project og project manager

---
+++ Models/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eksamensprojekt_2nd.Models
+{
+    public class Project
+    {
+        public string? project_name { get; set; }
+        public string? project_number { get; set; }
+        public double hours_planed { get; set; }
+        public DateOnly start_date { get; set; }
+        public DateOnly end_date { get; set; }
+        public string? comment { get; set; }
+
+
+        //constructor
+        public Project() { }
+
+
+    }
+}
---
+++ Models/Project_manager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eksamensprojekt_2nd.Models
+{
+    class project_manager
+    {
+        public string? name { get; set; }
+        public string? employee_number { get; set; }
+        public string? phone_number { get; set; }
+        public string? email { get; set; }
+        public string? comment { get; set; }
+        public double? hours_available_per_month { get; set; }
+
+
+        // Constructor
+        public project_manager() 
+        { 
+        }
+    }
+}
