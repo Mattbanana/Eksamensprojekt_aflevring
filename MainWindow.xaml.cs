7946f2e4cd18871ebc34dd28d8e7d83bea701be1
Mattbanana <matthiasbirchandersen@gmail.com>
2022-12-07 08:44:25 +0100

    har flyttet de midlertideige lister ind i de respektive views codebehind

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,21 +28,8 @@ namespace Eksamensprojekt_2nd
             InitializeComponent();
 
             
-           /// midlertidig hardcoded liste med projekter, til test af program. Skal komme fra sql server senere.
-            List<Project> projects = new List<Project>() ;
-
-            projects.Add(new Project("project1", "1", 300, "2015-01-02", "2016-02-11", "sur som fanden ham selv"));
-            projects.Add(new Project("project2", "2", 300, "2015-03-12", "2016-02-11", "glad for fransk nugat"));
-            projects.Add(new Project("project3", "3", 300, "2015-04-8", "2016-02-11", "knap så glad for fransk nugat"));
-
-
-            /// midlertidig hardcoded liste med projekt managers, til test af program. Skal komme fra sql server senere.
-            List<Project_manager> project_Managers = new List<Project_manager>();
-
-            project_Managers.Add(new Project_manager("project manager 1", "sk-45783", "+455694837", "fisk@g.com","generalt ret høj", 130));
-            project_Managers.Add(new Project_manager("project manager 2", "st-483", "+45556837", "fiskestang@g.com", "knap så glad for fransk nugat", 140));
-            project_Managers.Add(new Project_manager("project manager 2", "st-483", "+4555683745", "fiskestangfang@g.com", "nugat nugat nugat", 120));
-
+          
+           
 
 
         }
--- Views/Projects_view.xaml.cs
+++ Views/Projects_view.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Eksamensprojekt_2nd.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,13 @@ namespace Eksamensprojekt_2nd.Views
         public Projects_view()
         {
             InitializeComponent();
+
+            /// midlertidig hardcoded liste med projekter, til test af program. Skal komme fra sql server senere.
+            List<Project> projects = new List<Project>();
+
+            projects.Add(new Project("project1", "1", 300, "2015-01-02", "2016-02-11", "sur som fanden ham selv"));
+            projects.Add(new Project("project2", "2", 300, "2015-03-12", "2016-02-11", "glad for fransk nugat"));
+            projects.Add(new Project("project3", "3", 300, "2015-04-8", "2016-02-11", "knap så glad for fransk nugat"));
         }
     }
 }
