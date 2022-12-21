ef1e8b7ee8267258bdf705ef5949b08be0b74628
Mattbanana <101939679+Mattbanana@users.noreply.github.com>
2022-12-21 13:28:49 +0100

    har oprettet create manger view

---
+++ Views/Create_project_manager_window.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Eksamensprojekt_2nd.Views
+{
+    /// <summary>
+    /// Interaction logic for Create_project_manager_window.xaml
+    /// </summary>
+    public partial class Create_project_manager_window : Window
+    {
+        public Create_project_manager_window()
+        {
+            InitializeComponent();
+        }
+    }
+}
