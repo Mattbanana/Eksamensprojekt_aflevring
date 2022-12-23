95df1de18ddfa576f5924398712a8f9b1cf2185f
Mattbanana <101939679+Mattbanana@users.noreply.github.com>
2022-12-23 09:50:29 +0100

    stavefejl rette

--- Viewmodels/Projects_viewmodel.cs
+++ Viewmodels/Projects_viewmodel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Eksamensprojekt_2nd.Models;
+using Eksamensprojekt_2nd.Views;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,80 @@ namespace Eksamensprojekt_2nd.Viewmodels
 {
     public class Projects_viewmodel
     {
+        
+        private Project project_VM;
+
+        public string Name
+        {
+            get { return project_VM.Project_name; }
+            set
+            {
+                project_VM.Project_name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string Number
+        {
+            get { return project_VM.Project_number; }
+            set
+            {
+                project_VM.Project_number = value;
+                OnPropertyChanged("Number");
+            }
+        }
+
+        public double Hours_planed
+        {
+            get { return project_VM.Hours_planed; }
+            set
+            {
+                project_VM.Hours_planed = value;
+                OnPropertyChanged("Hours_planed");
+            }
+        }
+
+        public string Start_date
+        {
+            get { return project_VM.Start_date; }
+            set
+            {
+                project_VM.Start_date = value;
+                OnPropertyChanged("Start_date");
+            }
+        }
+
+        public string End_date
+        {
+            get { return project_VM.End_date; }
+            set
+            {
+                project_VM.End_date = value;
+                OnPropertyChanged("End_date");
+            }
+        }
+
+        public string Comment
+        {
+            get { return project_VM.Comment; }
+            set
+            {
+                project_VM.Comment = value;
+                OnPropertyChanged("Comment");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
     }
 }
