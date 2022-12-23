a19f17fab165971e1458ae137e82d27c0c9deceb
Mattbanana <101939679+Mattbanana@users.noreply.github.com>
2022-12-23 09:45:11 +0100

    tilføjet sql forbindelse til project class i model

--- Models/Project.cs
+++ Models/Project.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Eksamensprojekt_2nd.Models
 {
@@ -29,6 +30,35 @@ namespace Eksamensprojekt_2nd.Models
         
         
         }
+
+        //a method that saves the properties of project to a database
+        public void Save()
+        {
+            //connection string
+            string connectionString = "Server=10.56.8.37 ;Database=DB20;User Id=STUDENT20;Password= OPENDB_20;";
+            //sql query
+            string sql = "INSERT INTO Project (Project_name, Project_number, Hours_planed, Start_date, End_date, Comment) " +
+                         "VALUES (@Project_name, @Project_number, @Hours_planed, @Start_date, @End_date, @Comment)";
+            //connection
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            //command
+            SqlCommand command = new SqlCommand(sql, connection);
+            //parameters
+            command.Parameters.AddWithValue("@Project_name", Project_name);
+            command.Parameters.AddWithValue("@Project_number", Project_number);
+            command.Parameters.AddWithValue("@Hours_planed", Hours_planed);
+            command.Parameters.AddWithValue("@Start_date", Start_date);
+            command.Parameters.AddWithValue("@End_date", End_date);
+            command.Parameters.AddWithValue("@Comment", Comment);
+            //open connection
+            connection.Open();
+            //execute command
+            command.ExecuteNonQuery();
+            //close connection
+            connection.Close();
+        }
+
 
 
     }
