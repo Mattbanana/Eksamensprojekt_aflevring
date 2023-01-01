33fa6677335b0a681235531b84bb2c31b3273081
Mattbanana <101939679+Mattbanana@users.noreply.github.com>
2023-01-01 11:42:46 +0100

    udcommenteret kode som ikke er en del af afleveringen

--- Views/Project_managers_view.xaml.cs
+++ Views/Project_managers_view.xaml.cs
@@ -26,24 +26,11 @@ namespace Eksamensprojekt_2nd.Views
         {
             InitializeComponent();
 
-            //// init values for set up
-            //Project_manager dummy = new Project_manager("dummy", "dummy", "12345678", "dummy", "dummy", 123);
-            //dummy.CreateProjectManagerTableInDB();
- 
-            /// midlertidig hardcoded liste med projekt managers, til test af program. Skal komme fra sql server senere.
-            //List<Project_manager> project_Managers = new List<Project_manager>();
-
-            //project_Managers.Add(new Project_manager("project manager 1", "sk-45783", "+455694837", "fisk@g.com", "generalt ret høj", 130));
-            //project_Managers.Add(new Project_manager("project manager 2", "st-483", "+4556943434", "fiskestang@g.com", "knap så glad for fransk nugat", 140));
-            //project_Managers.Add(new Project_manager("project manager 2", "st-483", "+4555683745", "fiskestangfang@g.com", "nugat nugat nugat", 120));
-
-            
-            //manager_list.ItemsSource = project_Managers;
-
         }
 
         private void add_manager_button_Click(object sender, RoutedEventArgs e)
         {
+            //this event makes a new window pop-up, where the user can create a new project_manager
             Create_project_manager_window create_project_manager_window = new Create_project_manager_window();
             create_project_manager_window.Show();
             
--- Views/Projects_view.xaml.cs
+++ Views/Projects_view.xaml.cs
@@ -26,157 +26,160 @@ namespace Eksamensprojekt_2nd.Views
     public partial class Projects_view : UserControl
     {
         public Projects_view()
-            
         {
             
             InitializeComponent();
+            
+            projects_listview.ItemsSource = Project_repo.GetAllProjectTableDB();
+
+            
+            // //hjælpefunktioner til test:
+            
             //CreateProjectTableInDB();
             //List<Project> project_list = Generate_ProjectList_with_Data();
             //WritelistToDB(project_list);
 
-            List<Project> projects_list = new() ;
-            projects_list = Project_repo.GetAllProjectTableDB();
-            
-            projects_listview.ItemsSource = projects_list;
         }
 
-        public void CreateProjectTableInDB()
-        {
-            string connectionString = "Server=10.56.8.37;Database=DB20;User Id=STUDENT20;Password= OPENDB_20;";
-
-
-            //try catch
-
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                string sql = "CREATE TABLE Project_table(" +
-                    "PK_project int PRIMARY KEY IDENTITY," +
-                    "FK_project_manager int," +
-                    "Name nvarchar(100)," +
-                    "Project_ref nvarchar(100)," +
-                    "Hours_planed int," +
-                    "Start_date DateTime," +
-                    "End_date DateTime," +
-                    "Comment nvarchar(1000))";
-
-                SqlCommand command = new SqlCommand(sql, connection);
-                command.ExecuteNonQuery();
-
-            }
-
-            
-
-
-
-        }
 
         private void add_project_button_Click(object sender, RoutedEventArgs e)
         {
+            //this event makes a new window pop-up, where the user can create a new project
             Create_project_window create_project_window = new Create_project_window();
             create_project_window.Show();
         }
 
-        public List<Project> Generate_ProjectList_with_Data()
+        ////hjælpefunktioner til test:
+        //public void CreateProjectTableInDB()
+        //{
+        //    string connectionString = "Server=10.56.8.37;Database=DB20;User Id=STUDENT20;Password= OPENDB_20;";
 
-        {
-            List<Project> projects_list = new List<Project>();
 
-            foreach (int i in Enumerable.Range(1, 10))
-            {
+        //    //try catch
 
-                projects_list.Add(new Project(
-                    "Project_" + i,
-                    "Project_ID_" + i,
-                    new Random().Next(100, 150),
-                    GetRandomNearFutureDate(),
-                    GetRandomFarFutureDate(),
-                    "Comment_" + i));
-            }
 
-            return projects_list;
+        //    using (SqlConnection connection = new SqlConnection(connectionString))
+        //    {
+        //        connection.Open();
+        //        string sql = "CREATE TABLE Project_table(" +
+        //            "PK_project int PRIMARY KEY IDENTITY," +
+        //            "FK_project_manager int," +
+        //            "Name nvarchar(100)," +
+        //            "Project_ref nvarchar(100)," +
+        //            "Hours_planed int," +
+        //            "Start_date DateTime," +
+        //            "End_date DateTime," +
+        //            "Comment nvarchar(1000))";
 
-            static DateTime GetRandomNearFutureDate()
-            {
-                var today = DateTime.Today;
-                return today.AddDays(new Random().Next(1, 30));
-            }
+        //        SqlCommand command = new SqlCommand(sql, connection);
+        //        command.ExecuteNonQuery();
 
-            // Method to generate a random date in the far future
-            static DateTime GetRandomFarFutureDate()
-            {
-                var today = DateTime.Today;
-                return today.AddDays(new Random().Next(365, 730));
-            }
-        }
+        //    }
 
-        void WritelistToDB(List<Project> projects_list)
-        {
-            using (SqlConnection connection = new SqlConnection("Server=10.56.8.37;Database=DB20;User Id=STUDENT20;Password= OPENDB_20;"))
-            {
-                connection.Open();
 
-                //string sql = "INSERT INTO Test_table (Name,Project_ID,Hours_planed,Start_date,End_date,Comment) " +
 
-                //   "VALUES (@Project_name,@Project_ID, @Hours_planed, @Start_date, @End_date, @Comment)";
 
-                foreach (Project project in projects_list)
-                {
-                    string sql = "INSERT INTO Project_table (Name,Project_ID,Hours_planed,Start_date,End_date,Comment) " +
-                    "VALUES (@Project_name,@Project_ID, @Hours_planed, @Start_date, @End_date, @Comment)";
 
-                    SqlCommand command = new SqlCommand(sql, connection);
+        //}
+        //public List<Project> Generate_ProjectList_with_Data()
 
-                    command.Parameters.AddWithValue("@Project_name", project.Project_name);
-                    command.Parameters.AddWithValue("@Project_ID", project.Project_ref);
-                    command.Parameters.AddWithValue("@Hours_planed", project.Hours_planed);
-                    command.Parameters.AddWithValue("@Start_date", project.Start_date);
-                    command.Parameters.AddWithValue("@End_date", project.End_date);
-                    command.Parameters.AddWithValue("@Comment", project.Comment);
+        //{
+        //    List<Project> projects_list = new List<Project>();
 
-                    command.ExecuteNonQuery();
-                }
+        //    foreach (int i in Enumerable.Range(1, 10))
+        //    {
 
-            }
+        //        projects_list.Add(new Project(
+        //            "Project_" + i,
+        //            "Project_ID_" + i,
+        //            new Random().Next(100, 150),
+        //            GetRandomNearFutureDate(),
+        //            GetRandomFarFutureDate(),
+        //            "Comment_" + i));
+        //    }
 
-        }
+        //    return projects_list;
 
-        public static List<Project> GetAllProjecttablefromDB()
-        {
-            List<Project> projects_list_from_DB = new List<Project>();
-            string connectionString = "Server=10.56.8.37;Database=DB20;User Id=STUDENT20;Password= OPENDB_20;";
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
+        //    static DateTime GetRandomNearFutureDate()
+        //    {
+        //        var today = DateTime.Today;
+        //        return today.AddDays(new Random().Next(1, 30));
+        //    }
 
-                string sql = "SELECT * FROM Project_table";
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        //if (projects_list_from_DB. > 0)
-                        //{
-                        //    projects_list_from_DB.Clear();
-                        //}
-                        while (reader.Read())
-                        {
-                            Project project = new Project(
-                             reader.GetString(1),
-                             reader.GetString(2),
-                             reader.GetInt32(3),
-                             reader.GetDateTime(4),
-                             reader.GetDateTime(5),
-                             reader.GetString(6));
+        //    // Method to generate a random date in the far future
+        //    static DateTime GetRandomFarFutureDate()
+        //    {
+        //        var today = DateTime.Today;
+        //        return today.AddDays(new Random().Next(365, 730));
+        //    }
+        //}
 
-                            projects_list_from_DB.Add(project);
-                        }
-                    }
-                }
-            }
+        //void WritelistToDB(List<Project> projects_list)
+        //{
+        //    using (SqlConnection connection = new SqlConnection("Server=10.56.8.37;Database=DB20;User Id=STUDENT20;Password= OPENDB_20;"))
+        //    {
+        //        connection.Open();
 
-            return projects_list_from_DB;
-        }
+        //        //string sql = "INSERT INTO Test_table (Name,Project_ID,Hours_planed,Start_date,End_date,Comment) " +
+
+        //        //   "VALUES (@Project_name,@Project_ID, @Hours_planed, @Start_date, @End_date, @Comment)";
+
+        //        foreach (Project project in projects_list)
+        //        {
+        //            string sql = "INSERT INTO Project_table (Name,Project_ID,Hours_planed,Start_date,End_date,Comment) " +
+        //            "VALUES (@Project_name,@Project_ID, @Hours_planed, @Start_date, @End_date, @Comment)";
+
+        //            SqlCommand command = new SqlCommand(sql, connection);
+
+        //            command.Parameters.AddWithValue("@Project_name", project.Project_name);
+        //            command.Parameters.AddWithValue("@Project_ID", project.Project_ref);
+        //            command.Parameters.AddWithValue("@Hours_planed", project.Hours_planed);
+        //            command.Parameters.AddWithValue("@Start_date", project.Start_date);
+        //            command.Parameters.AddWithValue("@End_date", project.End_date);
+        //            command.Parameters.AddWithValue("@Comment", project.Comment);
+
+        //            command.ExecuteNonQuery();
+        //        }
+
+        //    }
+
+        //}
+
+        //public static List<Project> GetAllProjecttablefromDB()
+        //{
+        //    List<Project> projects_list_from_DB = new List<Project>();
+        //    string connectionString = "Server=10.56.8.37;Database=DB20;User Id=STUDENT20;Password= OPENDB_20;";
+        //    using (SqlConnection connection = new SqlConnection(connectionString))
+        //    {
+        //        connection.Open();
+
+        //        string sql = "SELECT * FROM Project_table";
+        //        using (SqlCommand command = new SqlCommand(sql, connection))
+        //        {
+        //            using (SqlDataReader reader = command.ExecuteReader())
+        //            {
+        //                //if (projects_list_from_DB. > 0)
+        //                //{
+        //                //    projects_list_from_DB.Clear();
+        //                //}
+        //                while (reader.Read())
+        //                {
+        //                    Project project = new Project(
+        //                     reader.GetString(1),
+        //                     reader.GetString(2),
+        //                     reader.GetInt32(3),
+        //                     reader.GetDateTime(4),
+        //                     reader.GetDateTime(5),
+        //                     reader.GetString(6));
+
+        //                    projects_list_from_DB.Add(project);
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    return projects_list_from_DB;
+        //}
 
 
     }
